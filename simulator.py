dd202a94df3e3a5d22ad059b9f140df748d97904
Arpit <146352002+arrpt@users.noreply.github.com>
2024-04-18 01:05:59 +0530

    implemented r type

--- simulator.py
+++ simulator.py
@@ -3,106 +3,133 @@ import sys
 
 f = open('test1.txt', 'r')
 data = f.readlines()
-abi2register = {
-    "x0": 0,
-    "x1": 0,
-    "x2": 0,
-    "x3": 0,
-    "x4": 0,
-    "x5": 0,
-    "x6": 0,
-    "x7": 0,
-    "x8": 0,
-    "x8": 0,
-    "x9": 0,
-    "x10": 0,
-    "x11": 0,
-    "x12": 0,
-    "x13": 0,
-    "x14": 0,
-    "x15": 0,
-    "x16": 0,
-    "x17": 0,
-    "x18": 0,
-    "x19": 0,
-    "x20": 0,
-    "x21": 0,
-    "x22": 0,
-    "x23": 0,
-    "x24": 0,
-    "x25": 0,
-    "x26": 0,
-    "x27": 0,
-    "x28": 0,
-    "x29": 0,
-    "x30": 0,
-    "x31": 0,
+register = {
+    "00000": '00000000000000000000000000000000',
+    "00001": '00000000000000000000000000000000',
+    "00010": '00000000000000000000000000000000',
+    "00011": '00000000000000000000000000000000',
+    "00100": '00000000000000000000000000000000',
+    "00101": '00000000000000000000000000000000',
+    "00110": '00000000000000000000000000000000',
+    "00111": '00000000000000000000000000000000',
+    "01000": '00000000000000000000000000000000',
+    "01001": '00000000000000000000000000000000',
+    "01010": '00000000000000000000000000000000',
+    "01011": '00000000000000000000000000000000',
+    "01100": '00000000000000000000000000000000',
+    "01101": '00000000000000000000000000000000',
+    "01110": '00000000000000000000000000000000',
+    "01111": '00000000000000000000000000000000',
+    "10000": '00000000000000000000000000000000',
+    "10001": '00000000000000000000000000000000',
+    "10010": '00000000000000000000000000000000',
+    "10011": '00000000000000000000000000000000',
+    "10100": '00000000000000000000000000000000',
+    "10101": '00000000000000000000000000000000',
+    "10110": '00000000000000000000000000000000',
+    "10111": '00000000000000000000000000000000',
+    "11000": '00000000000000000000000000000000',
+    "11001": '00000000000000000000000000000000',
+    "11010": '00000000000000000000000000000000',
+    "11011": '00000000000000000000000000000000',
+    "11100": '00000000000000000000000000000000',
+    "11101": '00000000000000000000000000000000',
+    "11110": '00000000000000000000000000000000',
+    "11111": '00000000000000000000000000000000',
 }
 
-def func(r):#binary in string -> x(int) in string
-    return f"x{int(r,2)}"#"100" 100 
+def int2binary(num: int) -> str:
+    return format(2**32 + num, 'b')[-32:]
 
-def sign_extend(value, bits):
-    mask = 1 << (bits - 1)
-    return (value & ((1 << bits) - 1)) - (value & mask)
+def binary2uint(binary: str) -> int:
+    return int(binary, 2)
+
+def binary2sint(binary: str) -> int:
+    if binary[0] == '0':
+        return int(binary, 2)
+    else:
+        return int(binary, 2) - (1 << 32)
+
 
 def r_add(data):
-    rd = func(data[-12:-7])
-    rs1 = func(data[-20:-15])
-    rs2 = func(data[-25:-20])
-    abi2register[rd] = abi2register[rs1] + abi2register[rs2]
-    return None
+    rd = data[-12:-7]
+    rs1 = data[-20:-15]
+    rs2 = data[-25:-20]
+    out = binary2sint(register[rs1]) + binary2sint(register[rs2])
+    register[rd] = int2binary(out)
+    return
 
 def r_sub(data):
-    rd = func(data[-12:-7])
-    rs1 = func(data[-20:-15])
-    rs2 = func(data[-25:-20])
-    abi2register[rd] = abi2register[rs1] - abi2register[rs2]
-    return None
-
-def r_sll(data):
-
-    return None
+    rd = data[-12:-7]
+    rs1 = data[-20:-15]
+    rs2 = data[-25:-20]
+    out = binary2sint(register[rs1]) - binary2sint(register[rs2])
+    register[rd] = int2binary(out)
+    return
 
 def r_slt(data):
-    rd = func(data[-12:-7])
-    rs1 = func(data[-20:-15])
-    rs2 = func(data[-25:-20])
-    if abi2register[rs1]<abi2register[rs2]:
-        abi2register[rd]=1
-    return None
+    rd = data[-12:-7]
+    rs1 = data[-20:-15]
+    rs2 = data[-25:-20]
+    if binary2sint(register[rs1]) < binary2sint(register[rs2]):
+        register[rd] = int2binary(1)
+    else:
+        register[rd] = int2binary(0)
+    return
 
 def r_sltu(data):
-    rd = func(data[-12:-7])
-    rs1 = func(data[-20:-15])
-    rs2 = func(data[-25:-20])
-    if abi2register[rs1] < abi2register[rs2] :
-        abi2register[rd] = 1 
-    return None
+    rd = data[-12:-7]
+    rs1 = data[-20:-15]
+    rs2 = data[-25:-20]
+    if binary2uint(register[rs1]) < binary2uint(register[rs2]):
+        register[rd] = int2binary(1)
+    else:
+        register[rd] = int2binary(0)
+    return
 
-def r_xor(data):
-    rd = func(data[-12:-7])
-    rs1 = func(data[-20:-15])
-    rs2 = func(data[-25:-20])
-    abi2register[rd] = abi2register[rs1] ^ abi2register[rs2]
-    return None
+def r_sll(data):
+    rd = data[-12:-7]
+    rs1 = data[-20:-15]
+    rs2 = data[-25:-20]
+    register[rd] = (register[rs1] + '0'*binary2uint(register[rs2][-5:]))[-32:]
+    return
 
 def r_srl(data):
-    return None
+    rd = data[-12:-7]
+    rs1 = data[-20:-15]
+    rs2 = data[-25:-20]
+    register[rd] = ('0'*binary2uint(register[rs2][-5:]) + register[rs1])[:32]
+    return
+
+def r_xor(data):
+    rd = data[-12:-7]
+    rs1 = data[-20:-15]
+    rs2 = data[-25:-20]
+    out = ""
+    for i in range(32):
+        out += str(int(register[rs1][i]) ^ int(register[rs2][i]))
+    register[rd] = out
+    return
 
 def r_or(data):
-    rd = func(data[-12:-7])
-    rs1 = func(data[-20:-15])
-    rs2 = func(data[-25:-20])
-    abi2register[rd] = abi2register[rs1] | abi2register[rs2]
-    return None
+    rd = data[-12:-7]
+    rs1 = data[-20:-15]
+    rs2 = data[-25:-20]
+    out = ""
+    for i in range(32):
+        out += str(int(register[rs1][i]) | int(register[rs2][i]))
+    register[rd] = out
+    return
 
 def r_and(data):
-    rd = func(data[-12:-7])
-    rs1 = func(data[-20:-15])
-    rs2 = func(data[-25:-20])
-    abi2register[rd] = abi2register[rs1] & abi2register[rs2]
-    return None
+    rd = data[-12:-7]
+    rs1 = data[-20:-15]
+    rs2 = data[-25:-20]
+    out = ""
+    for i in range(32):
+        out += str(int(register[rs1][i]) & int(register[rs2][i]))
+    register[rd] = out
+    return
 
 def i_lw(data):#doubt
     immd = sign_extend(int(data[-31:-20],2),12)
